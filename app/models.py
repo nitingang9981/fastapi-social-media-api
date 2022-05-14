84d6372f643b42fbb13075fe3afab20b51e8f2d8
NItin Gangasagar <nitin.gangasagar@gmail.com>
2022-05-13 20:52:44 -0700

    removing phone #

--- app/models.py
+++ app/models.py
@@ -22,7 +22,7 @@ class User(Base):
     email = Column(String, nullable=False, unique=True)
     password = Column(String, nullable=False)
     created_at = Column(TIMESTAMP(timezone=True),nullable=False, server_default = text('now()'))
-    phone_number = Column(String)
+    #phone_number = Column(String)
 
 class Votes(Base):
     __tablename__="votes"
