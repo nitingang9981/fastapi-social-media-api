aa9c4537f0f291586d0d8cf049c04975c4765bbb
NItin Gangasagar <nitin.gangasagar@gmail.com>
2022-05-15 18:11:00 -0700

    lets see

--- tests/conftest.py
+++ tests/conftest.py
@@ -13,7 +13,7 @@ from app.oauth2 import create_access_token
 from app import models
 
 
-SQLALCHEMY_DATABASE_URL = f'postgresql://{settings.database_username}:{settings.database_password}@{settings.database_hostname}:{settings.database_port}/{settings.database_name}_qa'
+SQLALCHEMY_DATABASE_URL = f'postgresql://{settings.database_username}:{settings.database_password}@{settings.database_hostname}:{settings.database_port}/{settings.database_name}_test'
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 
